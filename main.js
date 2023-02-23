ab241fba0a25f36f75405304dd0235d2d010c28f
sebastxx <91297447+sebastxx@users.noreply.github.com>
2023-02-23 14:51:03 -0300

    debugging and cleaning

---
+++ main.js
@@ -0,0 +1,10 @@
+let events = {
+    name: 'Birthday Party',
+    date: '01/01/2020',
+    description: 'It\'s my birthday party',
+    category: 'Birthday',
+    place: 'My house',
+    capacity: 100,
+    estimatedAssistance: 50,
+    price: 0,
+}
