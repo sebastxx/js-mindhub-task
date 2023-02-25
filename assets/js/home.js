4cda9108189a0d50ca059ef75e23ead180517aec
sebastxx <91297447+sebastxx@users.noreply.github.com>
2023-02-24 21:08:36 -0300

    setting up data.js in cards

---
+++ assets/js/home.js
@@ -0,0 +1,24 @@
+
+const container = document.querySelector('#event-cards');
+
+let cards = '';
+
+//todos los eventos
+
+for (const event of data.events) {
+    cards += `<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                    <div class="card text-center">
+                        <img src="${event.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${event.name}</h5>
+                            <p class="card-text">${event.description}</p>
+                            <div class="row">
+                                <p class="col-sm-6 mb-3 mb-sm-0">Price: $ ${event.price}</p>
+                                <a href="#" class="btn btn-primary col-sm-6 mb-3 mb-sm-0">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+}
+
+container.innerHTML = cards;
