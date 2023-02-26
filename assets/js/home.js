a69c1325dd247eeb730e8fd95dca3e68dde7ba10
sebastxx <91297447+sebastxx@users.noreply.github.com>
2023-02-26 02:29:27 -0300

    fixed card's height and placement of buttons in cards

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,18 +3,20 @@ const container = document.querySelector('#event-cards');
 
 let cards = '';
 
-//todos los eventos
+// Cards para todos los eventos:
 
 for (const event of data.events) {
-    cards += `<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    cards += `  <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
                     <div class="card text-center">
                         <img src="${event.image}" class="card-img-top" alt="...">
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title">${event.name}</h5>
-                            <p class="card-text">${event.description}</p>
-                            <div class="row">
-                                <p class="col-sm-6 mb-3 mb-sm-0">Price: $ ${event.price}</p>
-                                <a href="#" class="btn btn-primary col-sm-6 mb-3 mb-sm-0">View more</a>
+                            <p class="card-text flex-grow-1">${event.description}</p>
+                            <div class="row mt-auto">
+                                <div class="col-sm-6 mb-3 mb-sm-0">Price: $ ${event.price}</div>
+                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <a href="#" class="btn btn-primary">View more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -23,20 +25,22 @@ for (const event of data.events) {
 
 container.innerHTML = cards;
 
-// // Seleccionar todas las cards
-// const cardsHeight = document.querySelectorAll('.card');
+// Regular altura de cards para que todas se vean iguales:
 
-// // Inicializar la altura más alta en 0
-// let maxHeight = 0;
+// Se seleccionan todas las cards con un QuerySelectorAll
+const cardsHeight = document.querySelectorAll('.card');
 
-// // Recorrer todas las cards y determinar la altura más alta
-// cardsHeight.forEach(card => {
-//   if (card.clientHeight > maxHeight) {
-//     maxHeight = card.clientHeight;
-//   }
-// });
+// Se setea la altura maxima en 0
+let maxHeight = 0;
 
-// // Establecer la misma altura en todas las cards
-// cardsHeight.forEach(card => {
-//   card.style.height = `${maxHeight}px`;
-// });
+//Se recorren todas las cards con un forEach y se determina la altura más alta
+cardsHeight.forEach(card => {
+    if (card.clientHeight > maxHeight) {
+    maxHeight = card.clientHeight;
+    }
+});
+
+// Se establece la misma altura en todas las cards
+cardsHeight.forEach(card => {
+    card.style.height = `${maxHeight}px`;
+});
--- assets/js/past.js
+++ assets/js/past.js
@@ -3,18 +3,20 @@ const container = document.querySelector('#event-cards');
 
 let cards = '';
 
-//eventospasados
+// Se traen sólo las cards para eventos pasados
 for (const event of data.events) {
     if (event.date < data.currentDate) {
-    cards += `<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+    cards += `  <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
                     <div class="card text-center">
                         <img src="${event.image}" class="card-img-top" alt="...">
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title">${event.name}</h5>
-                            <p class="card-text">${event.description}</p>
-                            <div class="row">
-                                <p class="col-sm-6 mb-3 mb-sm-0">Price: $ ${event.price}</p>
-                                <a href="#" class="btn btn-primary col-sm-6 mb-3 mb-sm-0">View more</a>
+                            <p class="card-text flex-grow-1">${event.description}</p>
+                            <div class="row mt-auto">
+                                <div class="col-sm-6 mb-3 mb-sm-0">Price: $ ${event.price}</div>
+                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <a href="#" class="btn btn-primary">View more</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -23,3 +25,23 @@ for (const event of data.events) {
 }
 
 container.innerHTML = cards;
+
+// Regular altura de cards para que todas se vean iguales:
+
+// Se seleccionan todas las cards con un QuerySelectorAll
+const cardsHeight = document.querySelectorAll('.card');
+
+// Se setea la altura maxima en 0
+let maxHeight = 0;
+
+//Se recorren todas las cards con un forEach y se determina la altura más alta
+cardsHeight.forEach(card => {
+    if (card.clientHeight > maxHeight) {
+    maxHeight = card.clientHeight;
+    }
+});
+
+// Se establece la misma altura en todas las cards
+cardsHeight.forEach(card => {
+    card.style.height = `${maxHeight}px`;
+});
